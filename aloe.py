396d15d4c1ca39acea56333f6600cbc3e6fc06d1
Russell Jarvis <rjjarvis@asu.edu>
2017-06-11 18:18:19 +1000

    dev changes

---
+++ aloe.py
@@ -0,0 +1,10 @@
+import os
+os.system('curl -i "https://api.speciesplus.net/api/v1/taxon_concepts.xml?name=Aloe" -H "X-Authentication-Token:9SbQcSRVXSyuOJdv4yeXigtt" >> aloe.xml')
+os.system('cat aloe.xml')
+#import json
+from pprint import pprint
+import untangle
+
+with open('aloe.xml') as data_file:
+    aloe_data = untangle.parse(data_file)
+pprint(aloe_data)
