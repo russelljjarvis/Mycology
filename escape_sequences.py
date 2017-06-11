e73b776218dd9d1d99c43d7d370a43d26a1fd442
Hannah Noorda <hannah.noorda@gmail.com>
2017-06-11 15:45:25 +1000

    beginning

---
+++ escape_sequences.py
@@ -0,0 +1,14 @@
+
+mycena_interrupta = "I'm delicate, \n small and \\ an irredescent blue"
+Cordyceps_gunnii = "zombie fungi"
+Lactarious_deliciousus = "Edible pine \f mushroom, \b delicous"
+
+forest = "a place to\\ find \t fungi"
+garden = "another place \f to find \r fungi"
+
+print mycena_interrupta
+print Cordyceps_gunnii
+print Lactarious_deliciousus
+
+print forest
+print garden
