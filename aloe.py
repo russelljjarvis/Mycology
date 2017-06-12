18f879095aaa95a198b4488290c885ce449f3c9b
Russell Jarvis <rjjarvis@asu.edu>
2017-06-12 11:46:12 +1000

    dev changes

--- aloe.py
+++ aloe.py
@@ -8,6 +8,31 @@ headers = {
 params = (
     ('name', 'Aloe'),
 )
-data = requests.get('https://api.speciesplus.net/api/v1/taxon_concepts.json?name=Aloe', headers=headers)
-a_data=data.text
+data=[]
+
+data.append(requests.get('https://api.speciesplus.net/api/v1/taxon_concepts.json?name=Aloe', headers=headers))
+identification = data[0].json()['taxon_concepts'][0]['id']
+print(identification)
+exec_str=str("data.append(requests.get(")
+print(exec_str)
+
+exec_str2=str("'https://api.speciesplus.net/api/v1/taxon_concepts.json/:")+str(identification)+str("/distributions',headers=headers))")
+print(exec_str2)
+exec(exec_str+exec_str2)
+exec_str2=str("'https://api.speciesplus.net/api/v1/taxon_concepts.json/:")+str(identification)+str("/references',headers=headers))")
+exec(exec_str+exec_str2)
+exec_str2=str("'https://api.speciesplus.net/api/v1/taxon_concepts.json/:")+str(identification)+str("/eu_legislation',headers=headers))")
+exec(exec_str+exec_str2)
+exec_str2=str("'https://api.speciesplus.net/api/v1/taxon_concepts.json/:")+str(identification)+str("/cites_legislation',headers=headers))")
+exec(exec_str+exec_str2)
+
+print(len(data))
+for i in data:
+    print(i)
+    if '<Response [200]>' in str(i):
+        i.json()
+    if '<Response [404]>' in str(i):
+        "nothing found in this field"
+
+a_data=data[0].text
 pprint(a_data)
